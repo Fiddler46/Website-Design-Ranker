a583adfec3abeb5cc463b4d7a36c4f83ee82afa6
Fiddler46 <adhyaksh99@gmail.com>
2019-10-24 01:07:51 +0530

    Simplified regex string

--- colcode.py
+++ colcode.py
@@ -1,7 +1,8 @@
 import re
-#Parse through example CSS files and find the regex of colour codes. Then count all the colour codes and set it as a variable.
-with open('style.css', 'st') as f:
-    test_String = f.read()
-    regex = st'/#([a-fA-F0-9]{3}){1,2}\b/'
-    result = re.findall(regex, test_string)
+with open('style.css') as f:
+    f.readlines()
+    #regex = r'/#([a-fA-F0-9]{3}){1,2}\b/'
+    result = re.findall(r'/#([a-fA-F0-9]{3}){1,2}\b/', f)
     print(result)
+    
+#Parse through example CSS files and find the regex of colour codes. Then count all the colour codes and set it as a variable.
