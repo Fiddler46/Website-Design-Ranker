98da063cc603082265263e0c19a037755f5b4180
dharwish <drpmiro@gmail.com>
2019-11-13 22:10:29 +0530

    pending commits

--- colcode.py
+++ colcode.py
@@ -3,6 +3,6 @@ with open('style.css') as f:
     file_contents = f.read()
     regex = r':?.(#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3})'
     result = re.findall(regex, file_contents)
-    print(len(result))
+    print(result)
     
 #Parse through example CSS files and find the regex of colour codes. Then count all the colour codes and set it as a variable.
