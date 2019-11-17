e1b6f1ba2733ff7e0de1d1bc7a731ac233f563f0
Fiddler46 <adhyaksh99@gmail.com>
2019-11-17 15:47:03 +0530

    Added url parsing code via bs4 and requests

--- colcode.py
+++ colcode.py
@@ -1,8 +1,24 @@
+import urllib.request as req
+from bs4 import BeautifulSoup
 import re
+url = input('enter a full website address: ')
+
+html = req.urlopen(url) # request the initial page
+soup = BeautifulSoup(html, 'html.parser')
+for styles in soup.select('style'): # get in-page style tags
+    print('in page style:')
+    print(styles.string)
+
+for link in soup.find_all('link', type='text/css'): # get links to external style sheets
+    address = link['href'] # the address of the stylesheet
+    if address.startswith('/'): # relative link
+        address = url + address
+    css = req.urlopen(address).read() # make a request to download the stylesheet from the address
+    print('linked stylesheet')
+    print(css)
 with open('style.css') as f:
     file_contents = f.read()
     regex = r':?.(#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3})'
     result = re.findall(regex, file_contents)
-    print(len(set(result)))
+    print((set(result)))
     
-#Parse through example CSS files and find the regex of colour codes. Then count all the colour codes and set it as a variable.
