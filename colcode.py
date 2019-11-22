135a8ae929e9ccd1e4ece92b5af2aa8c85d9fef6
dharwish <drpmiro@gmail.com>
2019-11-22 10:48:40 +0530

    Result writes to file and removed useless variable

--- colcode.py
+++ colcode.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import scrapy
-=======
 import urllib.request as req
 from bs4 import BeautifulSoup
->>>>>>> 810b893ac3b8d239a02f1afc7c29528f50018ff1
-=======
->>>>>>> fcddf4d7f489085668c212297db6be855fa7cf14
 import re
-
 import requests
 from bs4 import BeautifulSoup
-
 REGEX_CSS = re.compile(r':?.(#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3})')
 
 def main():
@@ -23,19 +14,12 @@ def main():
     for styles in soup.select('style'): # get in-page style tags
         print('in page style:')
         print(styles.string)
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    print(result)
-=======
     css_urls = map(
     lambda u: base_url + u if u.startswith("/") else u,
     (l["href"] for l in soup.find_all('link', type='text/css') if l.get("href"))
 )
     for url in css_urls:
         css_request = requests.get(url)
->>>>>>> fcddf4d7f489085668c212297db6be855fa7cf14
-
         if css_request.status_code != 200:
             print("Could not fetch CSS!")
             continue
@@ -46,13 +30,8 @@ def main():
         print((set(result)))
         print(len(set(result)))
 
-<<<<<<< HEAD
-=======
-    print((set(result)))
-    print(len(set(result)))
->>>>>>> 810b893ac3b8d239a02f1afc7c29528f50018ff1
-    
-=======
+    with open('out_col.txt', 'w') as f:
+        print('Filename:', result, file=f)
 if __name__ == "__main__":
     main()
->>>>>>> fcddf4d7f489085668c212297db6be855fa7cf14
+
